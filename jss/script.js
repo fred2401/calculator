a3d36a0a9a39916a92a9448902eeb2f1f8d3a34a
frederick asacha <fredasacha2@gmail.com>
2020-02-13 17:50:19 +0300

    added a code for calculating bmi

--- jss/script.js
+++ jss/script.js
@@ -4,5 +4,14 @@ var add=function(numOne, numTwo){
      return numOne + numTwo;
     };
 var result=add(numOne, numTwo);
-var outPut="your answer is "+ result;
-alert(result);
+var outPut="their sum is "+ result;
+alert(outPut);
+
+var heightMetres=parseInt(prompt("enter your height in meters"));
+var weightKg=parseInt(prompt("enter your weight in Kg"));
+var bmi=function(heightMetres, weightKg){
+    return weightKg / (heightMetres * heightMetres);
+};
+var resultTwo=bmi(weightKg, heightMetres);
+var outPuttwo="your bmi is "+ resultTwo;
+alert(outPuttwo);
