fffd62d0dc0e15db46efa10438fbb9dcbceae2a7
frederick asacha <fredasacha2@gmail.com>
2020-02-13 17:31:20 +0300

    included the add code'

---
+++ jss/script.js
@@ -0,0 +1,8 @@
+var numOne=parseInt(prompt("input a number"));
+var numTwo=parseInt(prompt("input another number"));
+var add=function(numOne, numTwo){
+     return numOne + numTwo;
+    };
+var result=add(numOne, numTwo);
+var outPut="your answer is "+ result;
+alert(result);
